1fd6fc25680495aed005802e29f46bbe0613ecbb
Stephen Underwood <41934385+stephenunder@users.noreply.github.com>
2019-04-18 13:03:56 -0500

    create setToken func and return in createSessions

--- controllers/signin.js
+++ controllers/signin.js
@@ -33,11 +33,19 @@ const signToken = (email) => {
   return jwt.sign(jwtPayload, "JWT_SECRET", { expiresIn: "2 days" });
 }
 
+const setToken = (key, value) => {
+  return Promise.resolve(redisClient.set(key, value));
+}
+
 const createSessions = (user) => {
   // JWT Token, return user data
   const { email, id } = user;
   const token = signToken(email);
-  return { success: "true", userId: id, token }
+  return setToken(token, id)
+    .then(() => {
+      return {success: "true", userId: id, token }
+    })
+    .catch(console.log)
 }
 
 const signinAuthentication = (db, bcrypt) => (req, res) => {
